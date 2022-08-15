9f40da1fd27156e8636e82748b59131c65c63f29
rfolk4835 <reid_folk@yahoo.ca>
2022-08-15 04:30:21 -0700

    Implemented NUnit testing framework

---
+++ tests/PrimeService.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using Prime.Services;
+
+namespace PrimeService.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
