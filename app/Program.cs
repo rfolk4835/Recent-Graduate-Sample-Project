b09b2b5e2ea5d2ae6e349c3758c21f43bcc4b3fe
rfolk4835 <reid_folk@yahoo.ca>
2022-08-24 16:54:33 -0700

    Moved parcer to project app

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace JSONparcer {
+    public class parcer {
+        public static void Main(string[] args) {
+            var json = System.IO.File.ReadAllText(@"d:\data\data_model.json");
+            var objects = JArray.Parse(json);
+            foreach(JObject root in objects) {
+                foreach(KeyValuePair<String, JToken> app in root) {
+                   
+                }
+            }
+        }
+    }
+}
