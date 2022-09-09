345e01b9c84b548e529d8b4d1c55eb05bbd15968
rfolk4835 <reid_folk@yahoo.ca>
2022-09-09 15:24:34 -0700

    Serializer test, NullReferenceException

--- app/Program.cs
+++ app/Program.cs
@@ -1,25 +1,25 @@
 ﻿using System.Text.Json;
 
 namespace JSONparcer {
-    public class BookList {
-        public List<Book> list { get; set; }
-    }
     public class Book {
-        public string isbn { get; set; }
-        public string title { get; set; }
-        public string subtitle { get; set; }
-        public string author { get; set; }
-        public string published { get; set; }
-        public string publisher { get; set; }
-        public int pages { get; set; }
+        public string? isbn { get; set; }
+        public string? title { get; set; }
+        public string? subtitle { get; set; }
+        public string? author { get; set; }
+        public DateTime? published { get; set; }
+        public string? publisher { get; set; }
+        public int? pages { get; set; }
         public string description { get; set; }
         public string website { get; set; }
+    }
+    public class BookList {
+        public List<Book>? list { get; set; }
     }
     public class parcer {
         public static void Main(string[] args) {
             string fileName = "data/data_model.json";
             string jsonString = File.ReadAllText(fileName);
-            BookList books = new JsonSerializer().Deserialize<BookList>(jsonString);
+            BookList books = JsonSerializer.Deserialize<BookList>(jsonString);
 
             foreach( var item in books.list) {
                 Console.WriteLine("isbn: {0}", item.isbn);
